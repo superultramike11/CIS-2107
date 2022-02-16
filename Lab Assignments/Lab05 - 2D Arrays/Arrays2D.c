883f3fa8dbf6d4df4d8b0f2492cede6a6c72c486
superultramike11 <91653380+superultramike11@users.noreply.github.com>
2022-02-15 23:09:40 -0500

    Finished Lab 5

--- Lab Assignments/Lab05 - 2D Arrays/Arrays2D.c
+++ Lab Assignments/Lab05 - 2D Arrays/Arrays2D.c
@@ -12,30 +12,31 @@
 #include <stdbool.h>
 #include <time.h>
 
+// Defining static variables
 static int rows = 0;
 static int cols = 0;
 
+// Function prototypes
 int max(int array[][cols]);
 int rowSum(int array[][cols]);
 int columnSum(int array[][cols]);
 bool isSquare(int array[][cols]);
 void displayOutputs(int array[][cols]);
 
+// Main function
 int main(void) {
-
-
+    // Ask user for # of rows and cols
     puts("Let's create a 2Dim array!\n");
-
     printf("\tHow many rows? ");
     scanf("%d", &rows);
-
     printf("\tHow many colums? ");
     scanf("%d", &cols);
 
+    // Declare array
     int array[rows][cols];
 
+    // Ask user for array elements
     puts("");
-
     for(int i=0; i<rows; i++) {
         for(int j=0;j<cols;j++) {
             printf("\tenter [%d][%d]: ", i, j);
@@ -44,56 +45,82 @@ int main(void) {
     }
     puts("");
 
+    // Array function calls
+    max(array);
+    rowSum(array);
+    columnSum(array);
     isSquare(array);
-
     displayOutputs(array);
 
-    max(array);
 }
 
 // Returns the maximum value in the 2d array
 int max(int array[][cols]) {
     int max = 0;
-    for(int i=0; i<rows; i++) {
-        for(int j=0;j<cols;j++) {
+    for(int i=0; i<rows; ++i) {
+        for(int j=0;j<cols; ++j) {
             if(array[i][j] > max) {
                 max = array[i][j];
             }
         }
     }
-    printf("Max element is %d\n", max);
+    printf("Max element is %d\n\n", max);
     return max;
 }
 
 // Returns the sum of the elements in Row X of the 2d array
 int rowSum(int array[][cols]) {
-
+    int sum=0;
+    for(int i=0; i<rows; ++i) {
+        for(int j=0; j<cols; ++j) {
+            sum = sum + array[i][j];
+        }
+        printf("Sum of row %d = %d\n", i+1, sum);
+        sum = 0;
+    }
+    puts("");
+    return sum;
 }
 
 // Returns the sum of the elements in Column x of the 2d array
 int columnSum(int array[][cols]) {
-
+    int sum;
+    for(int i=0; i<cols; ++i) {
+        for(int j=0; j<rows; ++j) {
+            sum += array[j][i];
+        }
+        printf("Sum of column %d = %d\n", i+1, sum);
+        sum = 0;
+    }
+    puts("");
+    return sum;
 }
 
 // Checks if the array is square (every row has the same length as the 2d array itself)
 bool isSquare(int array[][cols]) {
     if(rows == cols) {
-        puts("This is a square array.");
+        puts("This is a square array.\n");
         return true;
     }
     else {
-        puts("This is not a square array");
+        puts("This is not a square array.\n");
         return false;
     }
 }
 
 // Displays the 2 dim-array elements
 void displayOutputs(int array[][cols]) {
-    puts("Printing 2D Array");
+    puts("Here is your 2Dim array:");
     for(int i=0; i<rows; i++) {
+        printf("%s", "[");
         for(int j=0;j<cols;j++) {
-            printf("%d ", array[i][j]);
+            if(j == cols - 1) {
+                printf("%d", array[i][j]);
+            }
+            else {
+                printf("%d, ", array[i][j]);
+            }
         }
-        puts("");
+        puts("]");
    }
 }
