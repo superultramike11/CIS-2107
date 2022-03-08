2bfe7204bb2d6efed2861495d1788114eeb720e7
superultramike11 <91653380+superultramike11@users.noreply.github.com>
2022-03-08 18:30:22 -0500

    fixed errors in Lab

--- Lab Assignments/Lab06 - Array of Pointers/ArraysofPointers.c
+++ Lab Assignments/Lab06 - Array of Pointers/ArraysofPointers.c
@@ -13,22 +13,40 @@ grades for individual students in a 2D matrix and determining the minimum, maxim
 // menu-driven interface. This interface is an array of pointers to the functions.
 
 */
-
+// Pre-process directives
 #include <stdio.h>
 #include <stdlib.h>
-#define STUDENTS 3
-#define EXAMS 4
 
-void printArray(int a, int grades[][EXAMS], int students, int exams);
-void minimum(int b, int grades[][EXAMS], int students, int exams);
-void maximum(int c, int students, int exams, int grades[][EXAMS]);
-void average(int d, int students, int exams, int grades[][EXAMS]);
+// Function prototypes
+void printArray(int students, int exams, int grades[students][exams]);
+void minimum(int students, int exams, int grades[students][exams]);
+void maximum(int students, int exams, int grades[students][exams]);
+void average(int students, int exams, int grades[students][exams]);
 
+// Main function
 int main() {
-    void (*f[4])(int) = {printArray, minimum, maximum, average};
-    int grades[STUDENTS][EXAMS] = { { 77, 68, 86, 73},
-                                    { 96, 87, 89, 78},
-                                    { 70, 90, 86, 81} };
+    void (*f[4])(int students, int exams, int grades[students][exams]) = {printArray, minimum, maximum, average};
+    int students, exams;
+
+    // Ask user for # of rows and cols
+    puts("Let's create a 2Dim array for the grades!");
+    printf("\tHow many students? ");
+    scanf("%d", &students);
+    printf("\tHow many exams for each student? ");
+    scanf("%d", &exams);
+
+    // Declare array
+    int grades[students][exams];
+
+    // Ask user for array elements
+    puts("");
+    for(size_t i=0; i<students; ++i) {
+        for(size_t j=0;j<exams; ++j) {
+            printf("\tenter [%d][%d]: ", i, j);
+            scanf("%d, ", &grades[i][j]);
+        }
+    }
+    puts("");
 
     printf("%s", "Enter a choice:\n\t0 Print the array of grades\n\t1 Find the minimum grade\n\t2 Find the maximum grade\n\t3 Print the average on all tests for each student\n\t4 End Program\n\t");
     int choice; // variable to hold user's choice
@@ -38,7 +56,7 @@ int main() {
     while(choice >= 0 && choice < 4) {
         // invoke function at location choice in array f and pass
         // choice as an argument
-        (*f[choice])(choice);
+        (*f[choice])(students, exams, grades);
 
         printf("%s", "Enter a choice:\n\t0 Print the array of grades\n\t1 Find the minimum grade\n\t2 Find the maximum grade\n\t3 Print the average on all tests for each student\n\t4 End Program\n\t");
         scanf("%d", &choice);
@@ -48,28 +66,23 @@ int main() {
 }
 
 // students are pupils, exams are tests
-void printArray(int a, int grades[][EXAMS], int students, int exams){
+void printArray(int students, int exams, int grades[students][exams]){
     puts("Here is your 2Dim array:");
-    for(size_t i=0; i<3; ++i) {
-        printf("%s", "[");
-        for(size_t j=0;j<4; ++j) {
-            if(j == exams - 1) {
-                printf("%d", grades[i][j]);
-            }
-            else {
-                printf("%d, ", grades[i][j]);
-            }
+    printf("# of students is: %d\n", students);
+    printf("# of exams is: %d\n", exams);
+    for(size_t i=0; i<students; ++i) {
+        for(size_t j=0;j<exams; ++j) {
+            printf("%d ", grades[i][j]);
         }
-        puts("]");
-   }
+    }
 }
 
-void minimum(int b, int grades[][EXAMS], int students, int exams){
-    printf("You entered %d so minimum was called\n\n", b);
+void minimum(int students, int exams, int grades[students][exams]){
+    puts("You entered %d so minimum was called\n\n");
 }
-void maximum(int c, int students, int exams, int grades[][EXAMS]){
-    printf("You entered %d so maximum was called\n\n", c);
+void maximum(int students, int exams, int grades[students][exams]){
+    puts("You entered %d so maximum was called\n\n");
 }
-void average(int d, int students, int exams, int grades[][EXAMS]){
-    printf("You entered %d so average was called\n\n", d);
+void average(int students, int exams, int grades[students][exams]){
+    puts("You entered %d so average was called\n\n");
 }
