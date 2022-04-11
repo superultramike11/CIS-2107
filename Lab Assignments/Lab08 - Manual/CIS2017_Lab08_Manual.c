3286253198a873091e0c99e95d511cf25824f9bf
superultramike11 <91653380+superultramike11@users.noreply.github.com>
2022-04-11 02:28:34 -0400

    Completed

--- Lab Assignments/Lab08 - Manual/CIS2017_Lab08_Manual.c
+++ Lab Assignments/Lab08 - Manual/CIS2017_Lab08_Manual.c
@@ -33,64 +33,82 @@ int main() {
     srand(time(NULL));
 
     //test for upperLower
+    puts("Test for upperLower");
     const char test[] = "This iS A Test";
     upperLower(test);
 
     //test for convertStrtoInt
-    printf("\n\nThe sum of 4 strings is: %d", convertStrtoInt("3", "4", "5", "6"));
+    puts("\n\nTest for convertStrtoInt");
+    printf("\tThe sum of 4 strings is: %d", convertStrtoInt("3", "4", "5", "6"));
 
     //test for convertStrtoFloat
-    printf("\n\nThe sum of 4 strings is: %.2f", convertStrtoFloat("3.5", "4.5", "5.5", "6.5"));
+    puts("\n\nTest for convertStrtoFloat");
+    printf("\tThe sum of 4 strings is: %.2f", convertStrtoFloat("3.5", "4.5", "5.5", "6.5"));
 
     //test for compareStr
+    puts("\n\nTest for compareStr");
     compareStr("Test1", "Test2");
 
     //test for comparePartialStr
+    puts("\n\nTest for comparePartialStr");
     comparePartialStr("Test1", "Test2", 4);
 
     //test for randomize
+    puts("\n\nTest for randomize");
     randomize();
 
     //test for tokenize number
+    puts("\nTest for tokenize number");
     char str[] = "(267) 436-6281";
     tokenizeTelNum(str);
 
     //test for reverse
+    puts("\n\nTest for reverse");
     char line[] = "Hello world";
     reverse(line);
 
     //test for countSubstr
+    puts("\n\nTest for countSubstr");
     char *line1 = "helloworldworld";
     char *substring = "world";
-    printf("\n\nNumber of Substrings %s inside %s: %d\n", substring, line1, countSubstr(line1, substring));
+    printf("\t# of Substrings %s inside %s: %d", substring, line1, countSubstr(line1, substring));
 
     //test for countChar
+    puts("\n\nTest for countChar");
     char w = 'w';
-    printf("\nNumber of character %c inside %s: %d\n", w, line1, countChar(line1, w));
+    printf("\t# of character %c inside %s: %d", w, line1, countChar(line1, w));
 
     //test for countAlpha
+    puts("\n\nTest for countAlpha");
     char str1[] = "Hello it's me.";
     countAlpha(str1);
 
     //test for countWords
+    puts("\nTest for countWords");
     char countstring[] = "hello world!";
-    printf("\n\nNumber of words in string is: %d\n", countWords(countstring));
+    printf("\tNumber of words in string is: %d", countWords(countstring));
 
     //test for startsWithB
+    puts("\n\nTest for startsWithB");
     char *series[] = {"bored", "hello", "Brother", "manual", "bothered"};
     int size = sizeof(series) / sizeof(series[0]);
     startsWithB(series, size);
 
     //test for endsWithed
+    puts("\nTest for endsWithed");
     endsWithed(series, size);
 }
 
 
 // 1.(Displaying Strings in Uppercase and Lowercase)
 void * upperLower (const char * s) {
-    while(*s != '\0') {
-        printf("%c", toupper(*s++));
+    char upper[100];
+    char lower[100];
+    for(int i=0; i<strlen(s); i++) {
+        upper[i] = toupper(s[i]);
+        lower[i] = tolower(s[i]);
     }
+    printf("\t%s %s", upper, lower);
 }
 
 // 2.(Converting Strings to Integers for Calculations)
@@ -107,11 +125,11 @@ float convertStrtoFloat(const char *s1, const char *s2, const char *s3, const ch
 void compareStr(const char *s1, const char *s2) {
     int comp = strcmp(s1, s2);
     if(comp < 0) {
-        puts("\nTest1 < Test2");
+        printf("\tTest1 < Test2");
     } else if (comp > 0) {
-        puts("\nTest1 > Test2");
+        printf("\tTest1 > Test2");
     } else {
-        puts("\nTest1 = Test2");
+        printf("\tTest1 = Test2");
     }
 }
 
@@ -119,11 +137,11 @@ void compareStr(const char *s1, const char *s2) {
 void comparePartialStr(const char *s1, const char *s2, int n) {
     int comp = strncmp(s1, s2, n);
     if(comp < 0) {
-        puts("\nComparison of first 4 chars: Test1 < Test2");
+        printf("\tComparison of first 4 chars: Test1 < Test2");
     } else if(comp > 0) {
-        puts("\nComparison of first 4 chars: Test1 > Test2");
+        printf("\tComparison of first 4 chars: Test1 > Test2");
     } else {
-        puts("\nComparison of first 4 chars: Test1 = Test2");
+        printf("\tComparison of first 4 chars: Test1 = Test2");
     }
 }
 
@@ -134,37 +152,38 @@ void randomize(void) {
     char *noun[] = {"boy", "girl", "dog", "town", "car"};
     char *verb[] = {"drove", "jumped", "ran", "walked", "skipped"};
     char *preposition[] = {"to", "from", "over", "under", "on"};
-
-
+    int s1, s2, s3, s4, s5, s6;
+    for(int i=0; i<20; i++) {
+        s1 = rand() % 5;
+        s2 = rand() % 5;
+        s3 = rand() % 5;
+        s4 = rand() % 5;
+        s5 = rand() % 5;
+        s6 = rand() % 5;
+        char first_to_upper = toupper(article[s1][0]);
+        printf("\t%c%s %s %s %s %s %s.\n", first_to_upper, article[s1] + 1, noun[s2], verb[s3], preposition[s4], article[s5], noun[s6]);
+    }
 }
 
 //7.(Tokenize Telephone Numbers)
 // (267) 436-6281
 int tokenizeTelNum(char *num) {
-    char area[4] = {};
-    char number[9] = {};
-    char *p = strtok(num, "()- ");
+    int area;
+    long number;
+    char *token, *area_code, *three, *four;
+    token = strtok(num, "(");
+    area_code = strtok(token, ")");
+    three = strtok(NULL, "-");
+    four = strtok(NULL, "");
 
-    if ( p != NULL ) {
-        strncat ( area, p, sizeof area - 1 );
-    }
+    char *seven = malloc(8);
+    strcpy(seven, three);
+    strcat(seven, four);
 
-    p = strtok ( NULL, "()- " );
+    area = atoi(area_code);
+    number = atol(seven);
 
-    if ( p != NULL ) {
-        strncat ( number, p, sizeof number - 1 );
-    }
-
-    p = strtok ( NULL, "()- " );
-
-    if ( p != NULL ) {
-        strncat ( number, p, sizeof number - 1 );
-    }
-
-    printf("Area code: %d", atoi(area));
-    //puts(area);
-    puts(number);
-    return 0;
+    printf("\t%d %ld", area, number);
 }
 
 //8.(Displaying a Sentence with Its Words Reversed)
@@ -173,7 +192,7 @@ void reverse(char *text) {
     for(int i=length-1; i>=0; i--) {
         if(text[i] == ' ') {
             text[i] = '\0';
-            printf("%s", &(text[i]) + 1);
+            printf("\t%s", &(text[i]) + 1);
         }
     }
     printf(" %s", text);
@@ -201,19 +220,12 @@ int countChar (char *line, char c) {
 
 //11.(Counting the Letters of the Alphabet in a String)
 void countAlpha(char *string) {
-    int count = 0;
-    int n = strlen(string);
-    for(int i=0; i<n; i++) {
-        count=1;
-        if(string[i]) {
-            for(int j=i+1; j<n; j++) {
-                if(string[i] == string[j]) {
-                    count++;
-                    string[j] = '\0';
-                }
-            }
-            printf(" '%c' = %d \n", string[i], count);
-        }
+    int alphabet[26];
+    char alpha = 'a';
+    for(int i=0; i<26; i++) {
+        alphabet[i] = countChar(string, alpha) + countChar(string, toupper(alpha));
+        printf("\t%c,%c = %d\n", toupper(alpha), alpha, alphabet[i]);
+        alpha++;
     }
 }
 
@@ -230,11 +242,10 @@ int countWords(char *string) {
 //13.(Strings Starting with "b")
 // "bored", "hello", "Brother", "manual", "bothered"
 void startsWithB(char *string[], int size) {
-    //char substr = 'b';
     for(int i=0; i<size; i++) {
         for(int j=0; j<strlen(string[i]); j++) {
             if(string[i][j] == 'B' || string[i][j] == 'b') {
-                printf("%s starts with b\n", string[i]);
+                printf("\t%s starts with b\n", string[i]);
             }
         }
     }
@@ -248,7 +259,7 @@ void endsWithed(char *string[], int size) {
     for(int i=0; i<size; i++) {
         for(int j=0; j<strlen(string[i]); j++) {
             if(string[i][j]=='e' && string[i][j+1] == 'd') {
-                printf("%s ends with ed\n", string[i]);
+                printf("\t%s ends with ed\n", string[i]);
             }
         }
     }
